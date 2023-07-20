85e1d9436b4e07c8675afa2bfbd59c21108be3a1
Doanvi123 <140040381+Doanvi123@users.noreply.github.com>
2023-07-20 12:11:17 +0700

    config.py

--- bot/config.py
+++ bot/config.py
@@ -2,13 +2,13 @@ import os
 
 class Config:
 
-    BOT_TOKEN = ""
+    BOT_TOKEN = "6180612482:AAEYZcSd4Gn4vaOeahDoxR2AHcqMGJTQj1o"
 
-    SESSION_NAME = ""
+    SESSION_NAME = "test6445"
 
-    API_ID = ""
+    API_ID = "15592273"
 
-    API_HASH = ""
+    API_HASH = "d9d8fe7f85ee6fb3798d9c1910ab249c"
 
     CLIENT_ID = ""
 
@@ -17,7 +17,7 @@ class Config:
     AUTH_USERS = [942731625]
 
     VIDEO_DESCRIPTION = ""
-
+    
     VIDEO_CATEGORY = ""
 
     VIDEO_TITLE_PREFIX = ""
